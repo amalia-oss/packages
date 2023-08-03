6cfdc00a09a3b9c25c936cdca84b2210c8d4e4a9
mlefebvre-amalia <maxence@amalia.io>
2023-08-03 14:58:41 +0200

    build: bump dependencies

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -1,12 +1,13 @@
 import { name, version } from '../package.json';
 
 import { objectPropertyNewlineRule } from './lib/rules/object-property-newline';
+import { Rule } from 'eslint';
 
 export const meta = {
   name,
   version,
 };
 
-export const rules = {
+export const rules: Record<string, Rule.RuleModule> = {
   'object-property-newline': objectPropertyNewlineRule,
 };
